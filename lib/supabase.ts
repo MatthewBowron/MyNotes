ebb9bc21d644c29c716cadd8f8e9e02e8b4cb282
vinicius berger <viniciuscla2015@gmail.com>
2025-08-11 15:14:40 -0600

    database bug fix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,17 @@
 import 'react-native-get-random-values';
 import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
     process.env.EXPO_PUBLIC_SUPABASE_URL,
-    process.env.EXPO_PUBLIC_SUPABASE_KEY
+    process.env.EXPO_PUBLIC_SUPABASE_KEY, 
+    { auth: {
+    storage: AsyncStorage,               // persist session on device
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,           // RN doesn’t use URL callbacks
+  }}
 );
 
 
